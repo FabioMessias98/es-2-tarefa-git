de7fdeb8fd57c85714a98d3f68aa8bb2c46755ae
Fabio Leandro <fabiomessias98@outlook.com>
2021-05-17 00:52:58 -0300

    adicionado os arquivos documentacao e index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<title>Es 2 - Git</title>
+		<meta charset="UTF-8">
+		<meta http-equiv="refresh" content=1>
+	</head>
+	<body>
+		<h1>
+			teste
+		</h1>
+	</body>
+</html>
