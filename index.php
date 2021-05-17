186c8aee333a7a0c7dd2bd12dc374b32f72772aa
Fabio Leandro <fabiomessias98@outlook.com>
2021-05-17 17:27:41 -0300

    adicionado o arquivo main.js

--- index.php
+++ index.php
@@ -56,7 +56,13 @@
 
 			<div class="container">
 
-				<div class="row justify-content-center">
+				<div class="row flex-column align-items-center">
+
+					<div class="col-12 mb-5">
+						<h2 class="text-center text-uppercase">
+							calcular a circunferência do circulo
+						</h2>
+					</div>
 
 					<div class="col-6">
 
@@ -101,6 +107,33 @@
 							</fieldset>
 						</form>
 					</div>
+
+					<div class="col-6 mt-4">
+						
+						<?php
+							$pi = 3.14;
+							$circle = new Circle();
+
+							if( isset($_POST['area']) ) 
+								$circle->setArea($_POST['area']);		
+							
+
+							$resultadoArea = $circle->calcularArea($circle->getArea(), $pi);
+						?>
+
+						<div class="row">
+
+							<div class="col-6">
+
+								<span class="u-hr-green"></span>
+
+								<p class="mt-3"> 
+									<strong>Resultado da área: </strong>
+									<?php echo $resultadoArea; ?>
+								</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -108,21 +141,13 @@
 		<div class="m-5">
 
 			<?php
-				$pi = 3.14;
-				$circle = new Circle();
-
-				if( isset($_POST['area']) ) 
-					$circle->setArea($_POST['area']);		
 				
-
-				$resultadoArea = $circle->calcularArea($circle->getArea(), $pi);
-
-				echo 'Resultado área: ' . $resultadoArea;
 			?>
 		</div>
 
 		<!-- boostrapjs -->
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src="js/main.js"></script>
 	</body>
 </html>
